3ca4247ebed92ae5d00153882fec601c49386a6f
CharlesCarlstrom <cc113884@students.davenport.k12.ia.us>
2017-02-10 14:50:08 -0600

    update
    update

---
+++ Chapter 3/gameZonePage178/Card.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gameZonePage178;
+
+/**
+ * @author CC113884
+ *
+ */
+public class Card {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter 3/gameZonePage178/PickTwoCards.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gameZonePage178;
+
+/**
+ * @author CC113884
+ *
+ */
+public class PickTwoCards {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
